d31c35b13d15d313c56648c93ddc49418090572c
Tristan Elliott <tristanelliott7@gmail.com>
2020-10-06 20:02:01 -0300

    adding react-router-dom and starting navbar

---
+++ story-sea-react/src/Components/App/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import NavBar from '../NavBar'
+
+
+const App =()=>{
+
+  return(
+    <div>
+      <NavBar/>
+      <Switch>
+
+      </Switch>
+    </div>
+  )
+}
+
+export default App
---
+++ story-sea-react/src/Components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const NavBar =()=>{
+
+  return(
+    <h1>This is where the navbar will be</h1>
+  )
+}
+
+
+export default NavBar
